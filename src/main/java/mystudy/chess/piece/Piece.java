c459034e5da5e25d1917f8831004e6413535caf1
chocolaggibbiddori <didrkd684@naver.com>
2022-12-05 09:12:27 +0900

    feat : Piece - 팀 변수 추가

--- src/main/java/mystudy/chess/piece/Piece.java
+++ src/main/java/mystudy/chess/piece/Piece.java
@@ -8,10 +8,12 @@ public abstract class Piece {
 
     Point point;
     final String teamName;
+    final boolean isTeamWhite;
 
     public Piece(Point point, String teamName) {
         this.point = point;
         this.teamName = teamName;
+        isTeamWhite = teamName.equals("White");
     }
 
     @Override
