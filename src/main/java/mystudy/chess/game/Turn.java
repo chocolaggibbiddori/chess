dcb8c3a0fbb8de97be4217ca8cf301b14cb64668
chocolaggibbiddori <didrkd684@naver.com>
2022-12-07 22:55:14 +0900

    feat : Turn - 생성

---
+++ src/main/java/mystudy/chess/game/Turn.java
@@ -0,0 +1,42 @@
+package mystudy.chess.game;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mystudy.chess.board.BoardRepository;
+import mystudy.chess.piece.Piece;
+import mystudy.chess.point.Point;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Turn {
+
+    private static final List<Notation> notationList = new ArrayList<>();
+    private static String currentTeam = "White";
+    private static int turnCount = 1;
+
+    private final BoardRepository boardRepository;
+    private final Notation notation;
+
+    public void turnCount() {
+        turnCount++;
+    }
+
+    public int getTurnCount() {
+        return turnCount;
+    }
+
+    public void changeCurrentTeam() {
+        currentTeam = currentTeam.equals("White") ? "Black" : "White";
+    }
+
+    public String getCurrentTeam() {
+        return currentTeam;
+    }
+
+    // TODO: 2022/12/06 기보 보여주기
+
+}
