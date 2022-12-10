0180cf0bf94748d3bd10828c81635ffba378d1b3
chocolaggibbiddori <didrkd684@naver.com>
2022-12-10 18:31:48 +0900

    feat : GameController - select 추가

--- src/main/java/mystudy/chess/controller/GameController.java
+++ src/main/java/mystudy/chess/controller/GameController.java
@@ -3,12 +3,16 @@ package mystudy.chess.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mystudy.chess.game.GameSetting;
+import mystudy.chess.piece.Piece;
+import mystudy.chess.point.Point;
 import mystudy.chess.repository.BoardRepository;
 import mystudy.chess.view.View;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -25,6 +29,17 @@ public class GameController {
         gameSetting.gameInit();
         String board = view.drawBoard();
         model.addAttribute("board", board);
-        return "/game";
+        log.info("Game Start");
+        return "/select";
+    }
+
+    @PostMapping("/select")
+    public String select(@RequestParam String strPiece, Model model) {
+        Point point = new Point(strPiece);
+        Piece piece = boardRepository.findByPoint(point);
+        log.info("Select Point, Piece : {}, {}", point, piece);
+        String board = view.drawBoard(piece.moveList());
+        model.addAttribute("board", board);
+        return "/move";
     }
 }
